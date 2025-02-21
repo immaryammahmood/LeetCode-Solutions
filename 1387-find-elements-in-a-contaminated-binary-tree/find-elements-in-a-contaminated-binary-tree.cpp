d155467bf3f62add8aba5fda406ccbae2386a8e1
Maryam <bsf23006580@ue.edu.pk>
2025-02-22 01:39:43 +0500

    Time: 8 ms (73.51%) | Memory: 27.3 MB (77.43%) - LeetSync

---
+++ 1387-find-elements-in-a-contaminated-binary-tree/find-elements-in-a-contaminated-binary-tree.cpp
@@ -0,0 +1,29 @@
+class FindElements {
+    unordered_set<int> seen;
+
+public:
+    FindElements(TreeNode* root) { bfs(root); }
+
+    bool find(int target) { return seen.find(target) != seen.end(); }
+
+private:
+    void bfs(TreeNode* root) {
+        queue<TreeNode*> queue;
+        root->val = 0;
+        queue.push(root);
+
+        while (!queue.empty()) {
+            TreeNode* currentNode = queue.front();
+            queue.pop();
+            seen.insert(currentNode->val);
+            if (currentNode->left) {
+                currentNode->left->val = currentNode->val * 2 + 1;
+                queue.push(currentNode->left);
+            }
+            if (currentNode->right) {
+                currentNode->right->val = currentNode->val * 2 + 2;
+                queue.push(currentNode->right);
+            }
+        }
+    }
+};
