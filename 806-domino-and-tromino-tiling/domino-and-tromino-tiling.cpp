86090245ed1688e396e3179695f45a6bb132276a
Maryam <bsf23006580@ue.edu.pk>
2025-05-08 02:24:34 +0500

    Time: 0 ms (100.00%) | Memory: 7.9 MB (77.73%) - LeetSync

--- 806-domino-and-tromino-tiling/domino-and-tromino-tiling.cpp
+++ 806-domino-and-tromino-tiling/domino-and-tromino-tiling.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
-    int mod = 1e9 + 7;
     int numTilings(int n) {
-        if (n <= 1) return 1;
-        if (n == 2) return 2;
-        if (n == 3) return 5;
-        vector<int> dp(n + 1, 0);
-        dp[0] = 1, dp[1] = 1, dp[2] = 2, dp[3] = 5;
-        for (int i = 4; i <= n; i++) 
-            dp[i] = (dp[i - 1] * 2 + long(dp[i - 3])) % mod;
-        
-        return dp[n];
+        if(n<=2)return n;
+        if(n==3)return 5;
+        int MOD=1e9+7;
+        long long a1=1,a2=2,a3=5;
+        for(int i=4;i<=n;i++){
+            long long curr = (2 * a3 + a1) % MOD;
+            a1=a2;
+            a2=a3;
+            a3=curr;
+        }
+        return (int)a3;
     }
 };
